3490d62925b14dc45b26a849bc697ff2554636fd
emi <bluming_blue@icloud.com>
2022-03-26 05:36:16 +0900

    様々な変更を加えましたが、エラーが起きています。

---
+++ FourTasks/View.swift
@@ -0,0 +1,22 @@
+//
+//  View.swift
+//  FourTasks
+//
+//  Created by 横田瑛美 on 2022/03/25.
+//
+
+import RealmSwift
+
+class View: Object {
+    // カテゴリー１
+    @objc dynamic var category1 = ""
+
+    //カテゴリー２
+    @objc dynamic var category2 = ""
+
+    // カテゴリー３
+    @objc dynamic var category3 = ""
+    
+    //カテゴリー４
+    @objc dynamic var category4 = ""
+}
--- FourTasks/ViewController.swift
+++ FourTasks/ViewController.swift
@@ -6,13 +6,46 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var task1CategoryTextField: UITextField!
+    @IBOutlet weak var task2CategoryTextField: UITextField!
+    @IBOutlet weak var task3CategoryTextField: UITextField!
+    @IBOutlet weak var task4CategoryTextField: UITextField!
+    
+    let realm = try! Realm()
+    var task: View
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+                self.view.addGestureRecognizer(tapGesture)
+
+                task1CategoryTextField.text = task.category1
+                task2CategoryTextField.text = task.category2
+                task3CategoryTextField.text = task.category3
+                task4CategoryTextField.text = task.category4
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+            try! realm.write {
+                self.task.category1 = self.task1CategoryTextField.text!
+                self.task.category2 = self.task2CategoryTextField.text!
+                self.task.category3 = self.task3CategoryTextField.text!
+                self.task.category4 = self.task4CategoryTextField.text!
+                self.realm.add(self.task, update: .modified)
+            }
+
+            super.viewWillDisappear(animated)
+        }
+
+        @objc func dismissKeyboard(){
+            // キーボードを閉じる
+            view.endEditing(true)
+        }
     
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
         }
